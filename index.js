c97dc170147cbb9fe209b5249c6298d4c0ba012b
Daniel Carey <danjcarey@outlook.com>
2020-08-10 20:42:26 +0100

    Redo, working much better now
    uses setInterval for keyframes, abstracted and density physics is working. Need to get the physics the right way around though, and then also add forces etc

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ class Particle {
         this.size = size;
         this.initX = this.x;
         this.initY = this.x;
-        this.mass = (Math.random()*10+1);
+        this.density = (Math.random()+1);
+        this.mass = this.density * this.size;
     }
 }
-
-particles = [];
 
 const height = document.getElementById("container").clientHeight;
 const width = document.getElementById("container").clientWidth;
 
-for(let i=0;i<20;i++){
-    particles.push(new Particle(Math.random()*width, Math.random()*height,3))
+let mousePos = {x:width/2,y:height/2};
+let particles = [];
+
+for(let i=0;i<40;i++){
+    particles.push(new Particle(Math.random()*width, Math.random()*height,Math.random()*5+1))
+    // console.log(particles[i]);
 }
- 
+
+let container = d3.select("#container");
+let circs = container.selectAll(".particle").data(particles);
+
+// the scales
 const x = d3.scaleLinear()
             .domain([0,2000])
             .range([0,width])
@@ -28,48 +35,48 @@ const y = d3.scaleLinear()
             .range([0,height])
             ;
 
-            
-function circles(){
-    let par = d3.select("#container")
-                .selectAll("circle")
+// update
+function updateElements(){        
+    container   .selectAll(".particle")
                 .data(particles)
                 .attr("cx", d => d.x)
                 .attr("cy", d=>d.y)
                 .attr("r", d=> d.size)
-                ;
-    let con = d3.select("#container");
-
-    par .enter()
-        .append("circle")
-        .classed("particle", true)
-        .attr("cx", d => d.x)
-        .attr("cy", d=>d.y)
-        .attr("r", d=> d.size)
-        .attr("fill", "#d587ad")
         ;
+}
+function createElements(){
+    container       .selectAll(".particle")
+                    .data(particles)
+                    .enter()
+                    .append("circle")
+                    .classed("particle", true)
+                    .attr("cx", d => d.x)
+                    .attr("cy", d=>d.y)
+                    .attr("r", d=> d.size)
+                    .attr("fill", "#d587ad")
 
-    par.exit().remove();
 }
 
-let con = d3.select("#container");
+createElements();
 
-con .on("click", function() {
-    let mouse = d3.mouse(this);     // 0 is x, 1 is y
-        for (let i in particles){
-            setTimeout(()=>{
-                let deltaX = particles[i].x - mouse[0];
-                let deltaY = particles[i].y - mouse[1];
-                particles[i].x = particles[i].x - (0.01*deltaX);
-                particles[i].y = particles[i].y - (0.01*deltaY);
-                console.log(deltaX);
-            }, 100);
-    
+container .on("mousemove", function(){
+    mousePos.x = d3.mouse(this)[0];
+    mousePos.y = d3.mouse(this)[1];
+});
+
+container.selectAll(".particles").exit().remove();
+
+setInterval(() => {
+    for(i in particles){
+        xDiff = (particles[i].x - mousePos.x)
+        yDiff = (particles[i].y - mousePos.y)
+        xDir = -xDiff/Math.abs(particles[i].x - mousePos.x);
+        yDir = -yDiff/Math.abs(particles[i].y - mousePos.y);
+        console.log(yDir)
+        if(Math.sqrt(xDiff*xDiff+yDiff*yDiff) < 300){
+            particles[i].x = particles[i].x + xDir/particles[i].mass*Math.sqrt(xDiff*xDiff+yDiff*yDiff)/100;
+            particles[i].y = particles[i].y + yDir/particles[i].mass*Math.sqrt(xDiff*xDiff+yDiff*yDiff)/100;
         }
-        circles();
-    })
-    .selectAll("circle")
-    .attr("cx", d => d.x)
-    .attr("cy", d=>d.y)
-    .attr("r", d=> d.size)
-    ;
-
+    }
+    updateElements();
+}, 5);
